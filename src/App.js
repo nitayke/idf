7dd4138f53d069c6c4311f9e0e27684a22893d2a
nitayke <nitayke@gmail.com>
2023-11-12 21:41:28 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import Quiz from "./Quiz";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/username/repo/branch/filename.json')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
       <h1 className="text-4xl mb-8">
