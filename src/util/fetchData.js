c061feaedea5c0a9de6939f768f91a87c81f8317
nitayke <nitayke@gmail.com>
2023-11-19 23:35:59 +0200

    correct green and red

--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -1,24 +1,26 @@
 import { QUESTIONS_COUNT } from "../App";
 
-export function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+export function shuffledArray(array) {
+    let copy = [...array];
+    for (let i = copy.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [copy[i], copy[j]] = [copy[j], copy[i]];
     }
+    return copy;
 }
 
 export async function getQuestions(level) {
     const url = `https://raw.githubusercontent.com/nitayke/idf/master/public/csv/${level}.csv`;
     const res = await fetch(url);
     const text = await res.text();
-    const questionsArr = text.split(", ");
-    shuffleArray(questionsArr);
+    let questionsArr = text.split(", ");
+    questionsArr = shuffledArray(questionsArr);
     let questions = [];
     for (let i = 0; i < QUESTIONS_COUNT; i++) {
         let question = [questionsArr[i]];
         let tmp = [...questionsArr];
         tmp.splice(i, 1);
-        shuffleArray(tmp);
+        tmp = shuffledArray(tmp);
         for (let j = 0; j < 3; j++) {
             question.push(tmp[j]);
         }
