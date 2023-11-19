3746e939ee98634c00d6a4ad517071f725c60e8f
nitayke <nitayke@gmail.com>
2023-11-19 22:24:01 +0200

    rtl loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 export default function Loader({ text }) {
   return (
     <>
-      <div className="loader-container">
+      <div className="loader-container" style={{ direction: "rtl" }}>
         <div className="loader"></div>
         <div className="loader__text">{text}</div>
       </div>
