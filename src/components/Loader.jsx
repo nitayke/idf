12125eb896b815b2e7de6234a2c4d1dd12b467be
nitayke <nitayke@gmail.com>
2023-11-19 21:44:58 +0200

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,8 @@
 export default function Loader({ text }) {
   return (
     <>
-      <div className="loader--container">
-        <div className="loader__circle"></div>
-        <div className="loader__line">
-          <div className="loader__clock"></div>
-        </div>
+      <div className="loader-container">
+        <div className="loader"></div>
         <div className="loader__text">{text}</div>
       </div>
     </>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,67 +1,63 @@
-import { useEffect, useState } from 'react';
-import Question from './Question';
-import { getQuestions } from '../util/fetchData';
-import Loader from './Loader';
-import EndGame from './EndGame';
+import { useEffect, useState } from "react";
+import Question from "./Question";
+import { getQuestions } from "../util/fetchData";
+import Loader from "./Loader";
+import EndGame from "./EndGame";
 
 export default function Quiz({ level, setLevel }) {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [score, setScore] = useState(0);
-    const [showScore, setShowScore] = useState(false);
-    const [questions, setQuestions] = useState([]);
-    const [isCorrect, setIsCorrect] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [isCorrect, setIsCorrect] = useState(false);
 
-    useEffect(() => {
-        (async function () {
-            setQuestions(await getQuestions(level));
-        })();
-    }, []);
+  useEffect(() => {
+    (async function () {
+      setQuestions(await getQuestions(level));
+    })();
+  }, []);
 
-    if (!questions.length) {
-        return <Loader text="טוען נתונים..."></Loader>;
+  if (!questions.length) return <Loader text="טוען נתונים..."></Loader>;
+
+  const handleAnswerSelected = (option) => {
+    if (option === questions[currentQuestionIndex][0]) {
+      setIsCorrect(true);
+      setScore(score + 1);
+    } else {
+      setIsCorrect(false);
     }
 
-    const handleAnswerSelected = (option) => {
-        if (option === questions[currentQuestionIndex][0]) {
-            setIsCorrect(true);
-            setScore(score + 1);
-        } else {
-            setIsCorrect(false);
-        }
-
-        const nextQuestionIndex = currentQuestionIndex + 1;
-        if (nextQuestionIndex >= questions.length) {
-            setShowScore(true);
-        }
-    };
-    return (
-        <div className="game">
-            {showScore ?
-                null
-                :
-                <div className="game--data">
-                    <p>שאלה {currentQuestionIndex + 1} מתוך 14</p>
-                    <p>הניקוד שלך בינתיים: {score}</p>
-                </div>
-            }
-            {showScore ?
-                <EndGame score={score} setLevel={setLevel} level={level}></EndGame>
-                :
-                <>
-                    {questions.map((question, index) => {
-                        return (
-                            <Question
-                                isCorrect={isCorrect}
-                                myIndex={index}
-                                currentQuestionIndex={currentQuestionIndex}
-                                setCurrentQuestionIndex={setCurrentQuestionIndex}
-                                options={question}
-                                onAnswerSelected={handleAnswerSelected}
-                            />
-                        )
-                    })}
-                </>
-            }
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if (nextQuestionIndex >= questions.length) {
+      setShowScore(true);
+    }
+  };
+  return (
+    <div className="game">
+      {showScore ? null : (
+        <div className="game--data">
+          <p>שאלה {currentQuestionIndex + 1} מתוך 14</p>
+          <p>הניקוד שלך בינתיים: {score}</p>
         </div>
-    );
+      )}
+      {showScore ? (
+        <EndGame score={score} setLevel={setLevel} level={level}></EndGame>
+      ) : (
+        <>
+          {questions.map((question, index) => {
+            return (
+              <Question
+                isCorrect={isCorrect}
+                myIndex={index}
+                currentQuestionIndex={currentQuestionIndex}
+                setCurrentQuestionIndex={setCurrentQuestionIndex}
+                options={question}
+                onAnswerSelected={handleAnswerSelected}
+              />
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
 }
